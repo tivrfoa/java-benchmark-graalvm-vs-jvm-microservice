810c224ec57f6a5baead7036bec6238164ab7043
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-06-02 10:51:12 -0300

    go fmt

--- go/client-api.go
+++ go/client-api.go
@@ -4,99 +4,98 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 // Address struct to hold address information
 type Address struct {
-    Street  string `json:"street"`
-    Number  int    `json:"number"`
-    State   string `json:"state"`
-    Country string `json:"country"`
+	Street  string `json:"street"`
+	Number  int    `json:"number"`
+	State   string `json:"state"`
+	Country string `json:"country"`
 }
 
 // Define the address data
 var address = Address{
-    Street:  "123 Main St",
-    Number:  42,
-    State:   "CA",
-    Country: "USA",
+	Street:  "123 Main St",
+	Number:  42,
+	State:   "CA",
+	Country: "USA",
 }
 
 // Function to handle GET requests for the address
 func getAddressHandler(w http.ResponseWriter, r *http.Request) {
-    // clientID := r.PathValue("client_id")
-    // Set content type as JSON
-    w.Header().Set("Content-Type", "application/json")
+	// clientID := r.PathValue("client_id")
+	// Set content type as JSON
+	w.Header().Set("Content-Type", "application/json")
 
-    // Marshal the address struct to JSON
-    jsonBytes, err := json.Marshal(address)
-    if err != nil {
-        fmt.Fprintf(w, "Error marshalling address to JSON: %v", err)
-        return
-    }
+	// Marshal the address struct to JSON
+	jsonBytes, err := json.Marshal(address)
+	if err != nil {
+		fmt.Fprintf(w, "Error marshalling address to JSON: %v", err)
+		return
+	}
 
-    // Write the JSON response
-    w.Write(jsonBytes)
+	// Write the JSON response
+	w.Write(jsonBytes)
 }
 
 type Phone struct {
-    DDD   int `json:"ddd"`
-    Number int `json:"number"`
+	DDD    int `json:"ddd"`
+	Number int `json:"number"`
 }
 
 var phones = [...]Phone{
-    {DDD: 11, Number: 987654321},
-    {DDD: 21, Number: 123456789},
-    {DDD: 22, Number: 987654320},
-    {DDD: 31, Number: 123456788},
-    {DDD: 32, Number: 987654319},
-    {DDD: 41, Number: 123456787},
-    {DDD: 42, Number: 987654318},
-    {DDD: 51, Number: 123456786},
-    {DDD: 52, Number: 987654317},
-    {DDD: 61, Number: 123456785},
-    {DDD: 62, Number: 987654316},
-    {DDD: 71, Number: 123456784},
-    {DDD: 72, Number: 987654315},
-    {DDD: 81, Number: 123456783},
-    {DDD: 82, Number: 987654314},
-    {DDD: 91, Number: 123456782},
-    {DDD: 92, Number: 987654313},
-    {DDD: 10, Number: 123456781},
-    {DDD: 10, Number: 987654312},
+	{DDD: 11, Number: 987654321},
+	{DDD: 21, Number: 123456789},
+	{DDD: 22, Number: 987654320},
+	{DDD: 31, Number: 123456788},
+	{DDD: 32, Number: 987654319},
+	{DDD: 41, Number: 123456787},
+	{DDD: 42, Number: 987654318},
+	{DDD: 51, Number: 123456786},
+	{DDD: 52, Number: 987654317},
+	{DDD: 61, Number: 123456785},
+	{DDD: 62, Number: 987654316},
+	{DDD: 71, Number: 123456784},
+	{DDD: 72, Number: 987654315},
+	{DDD: 81, Number: 123456783},
+	{DDD: 82, Number: 987654314},
+	{DDD: 91, Number: 123456782},
+	{DDD: 92, Number: 987654313},
+	{DDD: 10, Number: 123456781},
+	{DDD: 10, Number: 987654312},
 }
 
 // Function to handle GET requests for the list of telephones
 func getPhonesHandler(w http.ResponseWriter, r *http.Request) {
-    // clientID := r.PathValue("client_id")
-    // fmt.Printf("Handling client id: %s\n", clientID)
-    // Set content type as JSON
-    w.Header().Set("Content-Type", "application/json")
+	// clientID := r.PathValue("client_id")
+	// fmt.Printf("Handling client id: %s\n", clientID)
+	// Set content type as JSON
+	w.Header().Set("Content-Type", "application/json")
 
-    // Marshal the list of phones to JSON
-    jsonBytes, err := json.Marshal(phones)
-    if err != nil {
-        fmt.Fprintf(w, "Error marshalling telephones to JSON: %v", err)
-        return
-    }
+	// Marshal the list of phones to JSON
+	jsonBytes, err := json.Marshal(phones)
+	if err != nil {
+		fmt.Fprintf(w, "Error marshalling telephones to JSON: %v", err)
+		return
+	}
 
-    // Write the JSON response
-    w.Write(jsonBytes)
+	// Write the JSON response
+	w.Write(jsonBytes)
 }
 
 func main() {
-    // Register the handler for the GET request
-    http.HandleFunc("/address/{client_id}", getAddressHandler)
-    http.HandleFunc("/phones/{client_id}", getPhonesHandler)
+	// Register the handler for the GET request
+	http.HandleFunc("/address/{client_id}", getAddressHandler)
+	http.HandleFunc("/phones/{client_id}", getPhonesHandler)
 
-    // Start the server on port 8080
-    fmt.Println("Server listening on port 8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        panic(err)
-    }
+	// Start the server on port 8080
+	fmt.Println("Server listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
-
--- go/service.go
+++ go/service.go
@@ -4,274 +4,311 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "io"
-    "os"
-    "net/http"
-    "sync/atomic"
-    "github.com/jackc/pgx/v5"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"io"
+	"net/http"
+	"os"
+	"sync/atomic"
 )
 
 // Define a struct to hold client information
 type Client struct {
-    // ID          uint32  `json:"id"`
-    Name        string  `json:"name"`
-    Age         uint32  `json:"age"`
-    GuardianID  uint32  `json:"guardianID"`
-    MonthSalary float64 `json:"monthSalary"`
-    Address     Address `json:"address"`
-    Phones      []Phone `json:"phones"`
+	// ID          uint32  `json:"id"`
+	Name        string  `json:"name"`
+	Age         uint32  `json:"age"`
+	GuardianID  uint32  `json:"guardianID"`
+	MonthSalary float64 `json:"monthSalary"`
+	Address     Address `json:"address"`
+	Phones      []Phone `json:"phones"`
 }
 
 func NewClient(name string, age uint32, monthSalary float64) Client {
-  return Client{
-    // Set the relevant fields based on the provided arguments
-    Name:        name,
-    Age:         age,
-    MonthSalary: monthSalary,
-    // You can also set default values for other fields here if needed
-  }
+	return Client{
+		// Set the relevant fields based on the provided arguments
+		Name:        name,
+		Age:         age,
+		MonthSalary: monthSalary,
+		// You can also set default values for other fields here if needed
+	}
 }
 
 // Address struct to hold address information (same as before)
 type Address struct {
-    Street  string `json:"street"`
-    Number  int    `json:"number"`
-    State   string `json:"state"`
-    Country string `json:"country"`
+	Street  string `json:"street"`
+	Number  int    `json:"number"`
+	State   string `json:"state"`
+	Country string `json:"country"`
 }
 
 // Phone struct to hold phone number information (same as before)
 type Phone struct {
-    DDD    int `json:"ddd"`
-    Number int `json:"number"`
+	DDD    int `json:"ddd"`
+	Number int `json:"number"`
 }
 
 type LoanOption struct {
-  Years              int     `json:"years"`
+	Years int `json:"years"`
 
-  // Gemini suggested loanAmount, which is better! But let's keep the same as the Java version.
-  Loan               float64 `json:"loan"`
-  MonthlyInstallment float64 `json:"monthlyInstallment"`
+	// Gemini suggested loanAmount, which is better! But let's keep the same as the Java version.
+	Loan               float64 `json:"loan"`
+	MonthlyInstallment float64 `json:"monthlyInstallment"`
 }
 
 func NewLoanOption(years int, loan float64, monthlyInstallment float64) LoanOption {
-  return LoanOption{
-    Years:              years,
-    Loan:               loan,
-    MonthlyInstallment: monthlyInstallment,
-  }
+	return LoanOption{
+		Years:              years,
+		Loan:               loan,
+		MonthlyInstallment: monthlyInstallment,
+	}
 }
 
-
 func calculateLoanOptions(client Client) []LoanOption {
-  options := []LoanOption{}  // Empty slice to store LoanOption instances
-  for year := 1; year <= 3; year++ {
-    monthlyInstallment := 0.03 * client.MonthSalary  // Use dot notation for struct fields
-    maxLoan := monthlyInstallment * float64(12 * year)
-    interest := 0.07 * maxLoan
-    loanAmount := maxLoan - interest
-    options = append(options, NewLoanOption(year, loanAmount, monthlyInstallment))
-  }
-  return options
+	options := []LoanOption{} // Empty slice to store LoanOption instances
+	for year := 1; year <= 3; year++ {
+		monthlyInstallment := 0.03 * client.MonthSalary // Use dot notation for struct fields
+		maxLoan := monthlyInstallment * float64(12*year)
+		interest := 0.07 * maxLoan
+		loanAmount := maxLoan - interest
+		options = append(options, NewLoanOption(year, loanAmount, monthlyInstallment))
+	}
+	return options
 }
 
 type ResponseLoanOptions struct {
-  Client       Client      `json:"client"`
-  LoanOptions []LoanOption `json:"loanOptions"`
+	Client      Client       `json:"client"`
+	LoanOptions []LoanOption `json:"loanOptions"`
 }
 
 func NewResponseLoanOptions(client Client, loanOptions []LoanOption) ResponseLoanOptions {
-  return ResponseLoanOptions{
-    Client:       client,
-    LoanOptions: loanOptions,
-  }
+	return ResponseLoanOptions{
+		Client:      client,
+		LoanOptions: loanOptions,
+	}
 }
 
 // Define a constant slice of LoanOption to represent no loan options
-var NoLoanOptionsAvailable = []LoanOption{}  // Empty slice
+var NoLoanOptionsAvailable = []LoanOption{} // Empty slice
 
 var clients = []Client{
-  NewClient("A", 18, 3000),
-  NewClient("B", 19, 3500),
-  NewClient("C", 20, 4000),
-  NewClient("D", 21, 4500),
-  NewClient("E", 22, 5000),
-  NewClient("F", 23, 6000),
-  NewClient("G", 24, 7000),
-  NewClient("H", 25, 8000),
-  NewClient("I", 26, 9000),
-  NewClient("J", 27, 10000),
+	NewClient("A", 18, 3000),
+	NewClient("B", 19, 3500),
+	NewClient("C", 20, 4000),
+	NewClient("D", 21, 4500),
+	NewClient("E", 22, 5000),
+	NewClient("F", 23, 6000),
+	NewClient("G", 24, 7000),
+	NewClient("H", 25, 8000),
+	NewClient("I", 26, 9000),
+	NewClient("J", 27, 10000),
 }
 
 type Movie struct {
-  Title    string  `pg:"title,notnull"`
-  Year     int16   `pg:"year"`
-  Cost     float64 `pg:"cost"`
-  Director string  `pg:"director"`
+	Title    string  `pg:"title,notnull"`
+	Year     int16   `pg:"year"`
+	Cost     float64 `pg:"cost"`
+	Director string  `pg:"director"`
 }
 
 func testPostgresDb() {
-    postgresURL := "postgres://admin:123@localhost:5432/bench"
-    conn, err := pgx.Connect(context.Background(), postgresURL)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-        os.Exit(1)
-    }
-    defer conn.Close(context.Background())
+	postgresURL := "postgres://admin:123@localhost:5432/bench"
+	conn, err := pgx.Connect(context.Background(), postgresURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
-    // var name string
-    // err = conn.QueryRow(context.Background(), "select name from movie").Scan(&name)
-    // if err != nil {
-    //     fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-    // }
-    // fmt.Println(name)
-    rows, _ := conn.Query(context.Background(), "select title, year, cost, director from movie")
-    movies, err := pgx.CollectRows(rows, pgx.RowToStructByName[Movie])
-    if err != nil {
-        fmt.Printf("CollectRows error: %v", err)
-        return
-    }
+	// var name string
+	// err = conn.QueryRow(context.Background(), "select name from movie").Scan(&name)
+	// if err != nil {
+	//     fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// }
+	// fmt.Println(name)
+	rows, _ := conn.Query(context.Background(), "select title, year, cost, director from movie")
+	movies, err := pgx.CollectRows(rows, pgx.RowToStructByName[Movie])
+	if err != nil {
+		fmt.Printf("CollectRows error: %v", err)
+		return
+	}
 
-      // Create an empty map to store director names (set equivalent)
-    directorSet := map[string]struct{}{}
+	// Create an empty map to store director names (set equivalent)
+	directorSet := map[string]struct{}{}
 
-    for _, movie := range movies {
-        // fmt.Printf("%s: %s\n", movie.Title, movie.Director)
-        directorSet[movie.Director] = struct{}{} // Empty struct for value (doesn't matter)
-    }
+	for _, movie := range movies {
+		// fmt.Printf("%s: %s\n", movie.Title, movie.Director)
+		directorSet[movie.Director] = struct{}{} // Empty struct for value (doesn't matter)
+	}
 
-    // fmt.Println(directorSet)
-    // for directorName, _ := range directorSet {
-    //     fmt.Println(directorName)
-    // }
+	// fmt.Println(directorSet)
+	// for directorName, _ := range directorSet {
+	//     fmt.Println(directorName)
+	// }
+}
+
+func testPostgresDbPool() {
+	postgresURL := "postgres://admin:123@localhost:5432/bench"
+	pool, err := pgxpool.New(context.Background(), postgresURL)
+	// conn, err := pgx.Connect(context.Background(), postgresURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	// var name string
+	// err = conn.QueryRow(context.Background(), "select name from movie").Scan(&name)
+	// if err != nil {
+	//     fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// }
+	// fmt.Println(name)
+	rows, _ := conn.Query(context.Background(), "select title, year, cost, director from movie")
+	movies, err := pgx.CollectRows(rows, pgx.RowToStructByName[Movie])
+	if err != nil {
+		fmt.Printf("CollectRows error: %v", err)
+		return
+	}
+
+	// Create an empty map to store director names (set equivalent)
+	directorSet := map[string]struct{}{}
+
+	for _, movie := range movies {
+		// fmt.Printf("%s: %s\n", movie.Title, movie.Director)
+		directorSet[movie.Director] = struct{}{} // Empty struct for value (doesn't matter)
+	}
+
+	// fmt.Println(directorSet)
+	// for directorName, _ := range directorSet {
+	//     fmt.Println(directorName)
+	// }
 }
 
 func main() {
-    testPostgresDb()
+	testPostgresDb()
 
-    // Register the handler for the GET request
-    http.HandleFunc("/hello", getServiceHandler)
+	// Register the handler for the GET request
+	http.HandleFunc("/hello", getServiceHandler)
 
-    // Start the server on port 8080
-    fmt.Println("Server listening on port 8081")
-    err := http.ListenAndServe(":8081", nil)
-    if err != nil {
-        panic(err)
-    }
+	// Start the server on port 8080
+	fmt.Println("Server listening on port 8081")
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getServiceHandler(w http.ResponseWriter, r *http.Request) {
-    clientID := getClientId()
-    client := clients[clientID]
+	clientID := getClientId()
+	client := clients[clientID]
 
-    phones, err := getPhones(clientID)
-    if err != nil {
-        fmt.Println("Error fetching phones:", err)
-        return
-    }
-    client.Phones = phones
+	phones, err := getPhones(clientID)
+	if err != nil {
+		fmt.Println("Error fetching phones:", err)
+		return
+	}
+	client.Phones = phones
 
-    loanOptions := NoLoanOptionsAvailable
-    if client.Age >= 18 {
-        address, err := getAddress(clientID)
-        if err != nil {
-            fmt.Println("Error fetching address:", err)
-            return
-        }
-        client.Address = *address
-        loanOptions = calculateLoanOptions(client)
-    } else {
-        fmt.Println("BUG: this should be unreachable")
-        address, err := getAddress(client.GuardianID)
-        if err != nil {
-            fmt.Println("Error fetching address:", err)
-            return
-        }
-        client.Address = *address
-    }
+	loanOptions := NoLoanOptionsAvailable
+	if client.Age >= 18 {
+		address, err := getAddress(clientID)
+		if err != nil {
+			fmt.Println("Error fetching address:", err)
+			return
+		}
+		client.Address = *address
+		loanOptions = calculateLoanOptions(client)
+	} else {
+		fmt.Println("BUG: this should be unreachable")
+		address, err := getAddress(client.GuardianID)
+		if err != nil {
+			fmt.Println("Error fetching address:", err)
+			return
+		}
+		client.Address = *address
+	}
 
-    response := NewResponseLoanOptions(client, loanOptions)
-    jsonResponse, err := json.Marshal(response)
-    if err != nil {
-        fmt.Println("Error marshalling client data:", err)
-        return
-    }
+	response := NewResponseLoanOptions(client, loanOptions)
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Error marshalling client data:", err)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsonResponse)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
 
 func callAPI(url string) ([]byte, error) {
-    req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-        return nil, err
-    }
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    http_client := &http.Client{}
-    resp, err := http_client.Do(req)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	http_client := &http.Client{}
+	resp, err := http_client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
 
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return nil, err
-    }
-    return body, nil
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
 
 func getPhones(clientID uint32) ([]Phone, error) {
-    phonesURL := "http://localhost:8080/phones"
-    url := fmt.Sprintf("%s/%d", phonesURL, clientID)  // Format URL with client ID
+	phonesURL := "http://localhost:8080/phones"
+	url := fmt.Sprintf("%s/%d", phonesURL, clientID) // Format URL with client ID
 
-    body, err := callAPI(url)
-    if err != nil {
-        return nil, err
-    }
+	body, err := callAPI(url)
+	if err != nil {
+		return nil, err
+	}
 
-    var phones []Phone
-    err = json.Unmarshal(body, &phones)
-    if err != nil {
-        return nil, err
-    }
+	var phones []Phone
+	err = json.Unmarshal(body, &phones)
+	if err != nil {
+		return nil, err
+	}
 
-    return phones, nil
+	return phones, nil
 }
 
 func getAddress(clientID uint32) (*Address, error) {
-    addressURL := "http://localhost:8080/address"
-    url := fmt.Sprintf("%s/%d", addressURL, clientID)
+	addressURL := "http://localhost:8080/address"
+	url := fmt.Sprintf("%s/%d", addressURL, clientID)
 
-    body, err := callAPI(url)
-    if err != nil {
-        return nil, err
-    }
+	body, err := callAPI(url)
+	if err != nil {
+		return nil, err
+	}
 
-    var address Address
-    err = json.Unmarshal(body, &address)
-    if err != nil {
-        return nil, err
-    }
+	var address Address
+	err = json.Unmarshal(body, &address)
+	if err != nil {
+		return nil, err
+	}
 
-    return &address, nil
+	return &address, nil
 }
 
 // Created by Gemini. I hope it's correct xD
 var counter uint32 = 0
+
 func getClientId() uint32 {
-  for {
-    oldVal := atomic.LoadUint32(&counter)
-    newVal := (oldVal + 1) % 10
-    if atomic.CompareAndSwapUint32(&counter, oldVal, newVal) {
-      return oldVal  // Return the old value before increment
-    }
-  }
+	for {
+		oldVal := atomic.LoadUint32(&counter)
+		newVal := (oldVal + 1) % 10
+		if atomic.CompareAndSwapUint32(&counter, oldVal, newVal) {
+			return oldVal // Return the old value before increment
+		}
+	}
 }
