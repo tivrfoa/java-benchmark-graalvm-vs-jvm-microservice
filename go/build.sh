11cb91562280bcf7f5d60ace9a001f6be2cf433f
Leandro Coutinho <lescoutinhovr@gmail.com>
2024-06-02 13:33:35 -0300

    remove unused files

--- go/build.sh
+++
@@ -1,3 +0,0 @@
-go build -o bin/ client-api.go
-go build -o bin/ service.go
-
--- go/test-with-curl.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-    curl -s localhost:8080/address > /dev/null;
-    curl -s localhost:8080/phones > /dev/null;
-done
